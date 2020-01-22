75dd51d586c199c5ca62ae1829ee1f24455a2d7b
b0ntenmaru <mmst3l4@gmail.com>
2020-01-23 00:14:16 +0900

    デフォルトでコンソールにroutingを表示するようにした

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as listEndpoints from "express-list-endpoints";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+  console.log(listEndpoints(router));
 }
+
+
 bootstrap();
